3672886d1999ec190b475b76870d635439c1c5e8
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-03-14 18:29:39 +0200

    config-github-deploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode basename="/AirCondShop">
+  <React.StrictMode basename="/AirFresh">
     <App />
   </React.StrictMode>
 );
